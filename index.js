81cc874ea869a543b61fe8234d28e1138ae022ff
Roy Shmueli <2903801+2roy999@users.noreply.github.com>
2019-07-25 00:55:34 +0300

    Building a basic server

---
+++ index.js
@@ -0,0 +1,58 @@
+const Promise = require('bluebird')
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const winston = require('winston')
+
+const { main } = require('./lib/main')
+
+const app = express()
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'debug',
+  format: winston.format.json(),
+  defaultMeta: { service: 'server' },
+  transports: [
+    new winston.transports.Console()
+  ]
+})
+const port = process.env.PORT || 3000
+
+const mongoUri = process.env.MONGO_URI || 'mongodb://localhost:27017/fll-cloud'
+const mongoPromise = MongoClient.connect(mongoUri, {
+  promiseLibrary: Promise,
+  useNewUrlParser: true
+})
+
+const config = {
+  auth: {
+    google: {
+      clientId: process.env.GOOGLE_CONSUMER_KEY,
+      clientSecret: process.env.GOOGLE_CONSUMER_SECRET
+    },
+    callbackBaseUrl: process.env.AUTH_CALLBACK_BASE_URL,
+    superAdminEmail: process.env.SUPER_AMDIN_EMAIL
+  },
+  session: {
+    secret: 'secret'
+  }
+}
+
+const options = {
+  app,
+  db: mongoPromise.then(client => client.db()),
+  config,
+  logger,
+  mongo: mongoPromise
+}
+
+const entries = Object.entries(options)
+
+Promise.all(entries.map(([k, v]) => v))
+  .then(optionsArray => main(entries
+    .map(([k], index) => ({ [k]: optionsArray[index] }))
+    .reduce((obj, entry) => Object.assign(obj, entry), {})))
+  .then(() => app.listen(port, () => {
+    logger.info(`Server listens in port ${port}`)
+  }))
+  .catch(err => {
+    throw err
+  })
